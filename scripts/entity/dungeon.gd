8cd60c098c7f3dbf1e02d95c93de46a4cf44d40d
A. Roldán <aroldan.jdo@gmail.com>
2023-12-16 18:33:25 +0100

    Startup project

---
+++ scripts/entity/dungeon.gd
@@ -0,0 +1,22 @@
+# This file is part of Disnam.
+# 
+# Disnam is free software: you can redistribute it and/or modify it under the 
+# terms of the GNU General Public License as published by the Free Software 
+# Foundation, either version 3 of the License, or (at your option) any later 
+# version.
+# 
+# Disnam is distributed in the hope that it will be useful, but WITHOUT ANY 
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along with 
+# Disnam. If not, see <https://www.gnu.org/licenses/>
+
+
+class_name Dungeon
+extends RefCounted
+
+
+var width: int
+var height: int
+
